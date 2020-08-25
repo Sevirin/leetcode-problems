8ebc2c9180c06aa1db359aa7ae087c6ae016f581
Emil Nasstrom <ens19enm@cs.umu.se>
2020-08-25 15:25:00 +0200

    Medium

--- convertStringInKMoves.py
+++ convertStringInKMoves.py
@@ -7,7 +7,6 @@ def canConvertString(s: str, t: str, k: int) -> bool:
     for num, i in enumerate(t):
         index=letters.find(s[num])
         if i not in letters[index:index+k+1]:
-            print(t[index:index+k])
             return False
     return True
 s = "input"
